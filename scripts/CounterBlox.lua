60b891d8872785f7ef4ceb8b60b691f27b9b21be
GBrowser <guilfonseca2007@gmail.com>
2020-11-08 14:14:10 -0500

    commit by script

--- scripts/CounterBlox.lua
+++ scripts/CounterBlox.lua
@@ -2,11 +2,11 @@
 local config_lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/H3xad3cimalDev/Syntax-Hub/master/libs/config_handle.lib.lua", true));
 local uilib      = loadstring(game:HttpGet("https://raw.githubusercontent.com/H3xad3cimalDev/Syntax-Hub/master/libs/syntax_ui.lib.lua", true));
 
-local switchon = config_lib:GetSettings("switch");
+local switchon = config_lib:GetSetting("switch");
 if switchon == nil then
     config_lib:SetSetting("switch", false);
 end;
-switchon = config_lib:GetSettings("switch");
+switchon = config_lib:GetSetting("switch");
 
 local window = uilib:AddWindow("Test");
 local tab1   = window:AddTab("Test 1");
