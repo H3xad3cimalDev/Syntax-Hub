4fc4e945c3e36835bf16881f429e4db41b9a6b0e
GBrowser <guilfonseca2007@gmail.com>
2020-11-08 14:12:44 -0500

    commit by script

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,12 +1,14 @@
+-- Simple Bootstrap script that will auto load the script for the game
 local httpService = game:GetService("HttpService");
 local gameList    = httpService:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/H3xad3cimalDev/Syntax-Hub/master/info/gameList.json", true));
+local info        = gameList[tostring(game.PlaceId)];
 
-if gameList[tostring(game.PlaceId)] then
-
+if info then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/H3xad3cimalDev/Syntax-Hub/master/scripts/" .. info.Script, true))();
 else
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Syntax Hub Bootstrap",
-        Text  = "We're sorry for the issue but we couldn't find a script for the game " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId) .. ". :("
+        Text  = "We're sorry for the issue but we couldn't find a script for the game " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId) .. ". :(",
         Duration = 6,
         Button1 = "Ok"
     });
---
+++ scripts/CounterBlox.lua
@@ -0,0 +1,25 @@
+-- test for work
+local config_lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/H3xad3cimalDev/Syntax-Hub/master/libs/config_handle.lib.lua", true));
+local uilib      = loadstring(game:HttpGet("https://raw.githubusercontent.com/H3xad3cimalDev/Syntax-Hub/master/libs/syntax_ui.lib.lua", true));
+
+local switchon = config_lib:GetSettings("switch");
+if switchon == nil then
+    config_lib:SetSetting("switch", false);
+end;
+switchon = config_lib:GetSettings("switch");
+
+local window = uilib:AddWindow("Test");
+local tab1   = window:AddTab("Test 1");
+tab1:AddButton("Print", function()
+    print('test')
+end)
+local switch = tab1:AddSwitch("Test Save", function(s)
+    switchon = s;
+end)
+switch:Set(switchon);
+
+game:GetService("Players").PlayerRemoving:Connect(function(Player)
+    if Player == game:GetService("Players").LocalPlayer then
+        config_lib:Write2Config();
+    end
+end)
