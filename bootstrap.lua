9cbfe2955f6e39b61615a0482a820ba82901e0b1
GBrowser <guilfonseca2007@gmail.com>
2020-11-08 18:53:22 -0500

    commit by script

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,12 +1,10 @@
 -- Simple Bootstrap script that will auto load the script for the game
-print('Loading Bootstrap...');
 local httpService = game:GetService("HttpService");
 local gameList    = httpService:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/H3xad3cimalDev/Syntax-Hub/master/info/gameList.json", true));
 local info        = gameList[tostring(game.PlaceId)];
 
 if info ~= nil then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/H3xad3cimalDev/Syntax-Hub/master/scripts/" .. info.Script, true))();
-    print("Executed Script and Loaded Bootstrap!");
 else
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Syntax Hub Bootstrap",
@@ -14,5 +12,4 @@ else
         Duration = 6,
         Button1 = "Ok"
     });
-    print("Failed to Load Bootstrap.");
 end;
